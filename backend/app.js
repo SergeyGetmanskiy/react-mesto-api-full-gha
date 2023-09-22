0e152be804a05c89ac987248c18154e5cb93d449
Sergey Getmanskiy <getmansky1997@yandex.ru>
2023-09-22 15:37:00 +0300

    PR15 bug-fix

--- backend/app.js
+++ backend/app.js
@@ -53,10 +53,8 @@ app.get('/crash-test', () => {
 app.post('/signup', signupSchema, createUser);
 app.post('/signin', signinSchema, login);
 
-app.use(auth);
-
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
+app.use('/users', auth, require('./routes/users'));
+app.use('/cards', auth, require('./routes/cards'));
 
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Not found page' });
